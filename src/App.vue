9baf2bfbad63a71a64f907df66cd6798ed0471ce
Helio Marcondes <heliomsolivas@gmail.com>
2017-06-07 22:08:32 -0300

    Primeiro commit, v-for, componente inicial

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <card></card>
+</template>
+
+<script>
+
+import Card from './components/Card'
+
+export default {
+  components: {
+    Card
+  }
+}
+</script>
+
+<style>
+*{
+  box-sizing:border-box;
+  margin:0;
+  padding:0;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+	<div v-for="pokemon in pokemons" class="card">
+	<h1>{{pokemon.nome}}</h1>
+	<img v-bind:src="pokemon.foto" v-bind:alt="pokemon.nome">
+	<p>{{pokemon.descricao}}</p>
+</div>
+</div>
+</template>
+
+<script>
+
+//Standard Directives
+
+export default {
+  data() {
+  	return {
+    	pokemons: [
+    		{ nome: "Bulbasaur" , descricao: "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.", foto: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"},
+    		{ nome: "Ivysaur" , descricao: "There is a bud on this Pokémon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon.", foto: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png"}
+    			  ]
+    		}
+		}
+}
+</script>
+
+<style scoped>
+	.card{
+		width: 300px;
+		border:1px solid black;
+		border-radius: 10px;
+		padding: 10px 20px;
+		float: left;
+		height: 450px;
+	}
+	img{
+		max-width: 100%;
+		display: block;
+	}
+	h1{
+		font-size: 20px;
+	}
+	p{
+		font-size: 16px;
+	}
+</style>
